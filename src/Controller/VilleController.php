34f97986418986c69ea21d81a9f2d795b78f6a2f
ONS AYARI <onsayari93@gmail.com>
2023-07-28 11:42:06 +0200

    Ville type + Controller

--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -16,10 +16,6 @@ class VilleController extends AbstractController
     #[Route('/', name: 'app_ville_index', methods: ['GET'])]
     public function index(VilleRepository $villeRepository): Response
     {
-        if (!$this->getUser()) {
-            return $this->redirectToRoute('app_login');
-        }
-
         return $this->render('ville/index.html.twig', [
             'villes' => $villeRepository->findAll(),
         ]);
@@ -28,10 +24,6 @@ class VilleController extends AbstractController
     #[Route('/new', name: 'app_ville_new', methods: ['GET', 'POST'])]
     public function new(Request $request, VilleRepository $villeRepository): Response
     {
-        if (!$this->getUser()) {
-            return $this->redirectToRoute('app_login');
-        }
-
         $ville = new Ville();
         $form = $this->createForm(VilleType::class, $ville);
         $form->handleRequest($request);
@@ -39,5 +31,48 @@ class VilleController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $villeRepository->save($ville, true);
 
-            return $this
+            return $this->redirectToRoute('app_ville_index', [], Response::HTTP_SEE_OTHER);
         }
+
+        return $this->renderForm('ville/new.html.twig', [
+            'ville' => $ville,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_ville_show', methods: ['GET'])]
+    public function show(Ville $ville): Response
+    {
+        return $this->render('ville/show.html.twig', [
+            'ville' => $ville,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_ville_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Ville $ville, VilleRepository $villeRepository): Response
+    {
+        $form = $this->createForm(VilleType::class, $ville);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $villeRepository->save($ville, true);
+
+            return $this->redirectToRoute('app_ville_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('ville/edit.html.twig', [
+            'ville' => $ville,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_ville_delete', methods: ['POST'])]
+    public function delete(Request $request, Ville $ville, VilleRepository $villeRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$ville->getId(), $request->request->get('_token'))) {
+            $villeRepository->remove($ville, true);
+        }
+
+        return $this->redirectToRoute('app_ville_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
