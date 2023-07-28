e1d55116eedb4572426056220e068ffe8d53fed8
ONS AYARI <onsayari93@gmail.com>
2023-07-28 11:27:25 +0200

    Sortie Type

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -3,9 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Campus;
-use App\Entity\Participant;
 use App\Entity\Sortie;
-use http\Client\Curl\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
